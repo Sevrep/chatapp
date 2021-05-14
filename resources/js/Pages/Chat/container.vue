f3a536c7fe7ab2a52c3b28ebfc1d7f7c3d911803
Sevrep <62856034+Sevrep@users.noreply.github.com>
2021-05-14 14:18:06 +0800

    add message

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -7,7 +7,8 @@
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-          Container
+          <message-container />
+          <input-message :room="currentRoom" />
         </div>
       </div>
     </div>
@@ -16,10 +17,51 @@
 
 <script>
 import AppLayout from "@/Layouts/AppLayout";
+import MessageContainer from "./messageContainer.vue";
+import InputMessage from "./inputMessage.vue";
 
 export default {
   components: {
     AppLayout,
+    MessageContainer,
+    InputMessage,
+  },
+  data: function () {
+    return {
+      chatRooms: [],
+      currentRoom: [],
+      messages: [],
+    };
+  },
+  methods: {
+    getRooms() {
+      axios
+        .get("chat/rooms")
+        .then((response) => {
+          this.chatRooms = response.data;
+          this.setRoom(response.data[0]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    setRoom(room) {
+      this.currentRoom = room;
+      this.getMessages();
+    },
+    getMessages() {
+      axios
+        .get("/chat/room/" + this.currentRoom.id + "/messages")
+        .then((response) => {
+          this.messages = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getRooms();
   },
 };
 </script>
